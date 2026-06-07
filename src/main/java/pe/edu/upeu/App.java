068fb35e5e68ae00c0fb920af76ac2dcfa472724
Jhanpol <jhanpool799@gmail.com>
2026-06-07 15:23:57 -0500

    feat o hazana: create first javafx window

--- src/main/java/pe/edu/upeu/App.java
+++ src/main/java/pe/edu/upeu/App.java
@@ -1,13 +1,27 @@
 package pe.edu.upeu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage stage){
+        Label mensaje = new Label("Sea bienvenido a CompraVende");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(mensaje);
+
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Compravende");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+        //Stage = Marco de la casa
+        //Scene = Interior de la casa
     }
 }
